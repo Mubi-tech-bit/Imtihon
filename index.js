807197a89bf0aae85f94835f38232e408fca18a8
Mubi-tech-bit <rustamovraximberdi46@gmail.com>
2025-01-25 12:59:42 +0500

    Imtihon-15savolgacha

---
+++ index.js
@@ -0,0 +1,27 @@
+//11-topshiriq
+let ism = 'Shahzod'
+let yosh = 20
+let ball = 7
+//12-topshiriq
+console.log(ism);
+console.log(yosh);
+console.log(ball);
+//13-topshirq
+// console.log(); => bu ma'lumotlarni Console ekranida chiqaradi 
+let me = "Mubina"
+console.log(me);
+// alert => esa ma'lumotni bilidirshnoma singari aks ettiradi 
+alert(me)
+//14-topshiriq
+function Talaba(){
+    if(ball>6){
+        console.log("qabul qilindi");
+    }
+    else{
+        console.log("Qabul qilinmadi");
+    }
+}
+//15-topshiriq
+let sinf = ['Mubina', 'Omina', 'Gulshan']
+console.log(sinf);
+
